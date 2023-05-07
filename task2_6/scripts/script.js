812a20c3b9895e3e7026ef92e4eb11d5481c795a
Volodymyr Donets <volodons@gmail.com>
2023-05-07 09:37:50 +0300

    Add event listener and function to submit item to list

--- task2_6/scripts/script.js
+++ task2_6/scripts/script.js
@@ -4,3 +4,33 @@ const submitFormInput = document.getElementById("submitFormInput");
 const listItems = document.getElementById("listItems");
 const editButton = document.getElementById("editButton");
 const deleteAllButton = document.getElementById("deleteAllButton");
+
+// Add event listeners to buttons
+submitForm.addEventListener("submit", submitItem);
+
+// Submits an item to list
+function submitItem(event) {
+  event.preventDefault();
+  const itemText = submitFormInput.value;
+  const item = document.createElement("li");
+  item.classList.add("list__item");
+  item.innerHTML = `        
+    <span class="list__item-text">${itemText}</span>
+    <img
+      id="editButton"
+      class="list__item-icon"
+      src="./images/icon-edit.svg"
+      alt="Edit Item"
+      title="Edit Item"
+    />
+    <img
+      id="deleteButton"
+      class="list__item-icon"
+      src="./images/icon-delete.svg"
+      alt="Delete Item"
+      title="Delete Item"
+    />`;
+  listItems.append(item);
+  const deleteButton = document.getElementById("deleteButton");
+  deleteButton.addEventListener("click", deleteItem);
+}
