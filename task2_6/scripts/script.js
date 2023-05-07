3cc457b73c7380df7afa252f02c2c3ea630f5876
Volodymyr Donets <volodons@gmail.com>
2023-05-07 09:36:40 +0300

    Add variables to select input and buttons

---
+++ task2_6/scripts/script.js
@@ -0,0 +1,6 @@
+// Select input and buttons
+const submitForm = document.getElementById("submitForm");
+const submitFormInput = document.getElementById("submitFormInput");
+const listItems = document.getElementById("listItems");
+const editButton = document.getElementById("editButton");
+const deleteAllButton = document.getElementById("deleteAllButton");
