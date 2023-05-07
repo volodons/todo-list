1ab675c60801f343ac4f4922a0902d523cda83e6
Volodymyr Donets <volodons@gmail.com>
2023-05-07 17:57:58 +0300

    Create a function to edit and save edited items

--- task2_6/scripts/script.js
+++ task2_6/scripts/script.js
@@ -1,28 +1,29 @@
 // Select input and buttons
-const submitForm = document.getElementById("submitForm");
-const submitFormInput = document.getElementById("submitFormInput");
+const form = document.getElementById("form");
+const formInput = document.getElementById("formInput");
+const formButton = document.getElementById("formButton");
 const listItems = document.getElementById("listItems");
-const editButton = document.getElementById("editButton");
 const deleteAllButton = document.getElementById("deleteAllButton");
 
-// Add event listeners to buttons
-submitForm.addEventListener("submit", submitItem);
+// Add event listeners to form and button
+form.addEventListener("submit", addItem);
 deleteAllButton.addEventListener("click", deleteAllItems);
 
-// Counter for each new item
+// Set counter for each new item
 let counter = 0;
 
-// Submits an item to list
-function submitItem(event) {
+// Add new item to list
+function addItem() {
   counter++;
   event.preventDefault();
-  const itemText = submitFormInput.value;
-  const item = document.createElement("li");
+  let itemText = formInput.value;
+  let item = document.createElement("li");
+  item.id = `item${counter}`;
   item.classList.add("list__item");
   item.innerHTML = `        
     <span class="list__item-text">${itemText}</span>
     <img
-      id="editButton"
+      id="editButton${counter}"
       class="list__item-icon"
       src="./images/icon-edit.svg"
       alt="Edit Item"
@@ -36,8 +37,39 @@ function submitItem(event) {
       title="Delete Item"
     />`;
   listItems.append(item);
+  const editButton = document.getElementById(`editButton${counter}`);
+  editButton.addEventListener("click", () => editItem(item));
   const deleteButton = document.getElementById(`deleteButton${counter}`);
   deleteButton.addEventListener("click", () => deleteItem(item));
+}
+
+// Edit item
+function editItem(item) {
+  const allIcons = document.getElementsByTagName("img");
+  const allIconButtons = document.getElementsByClassName("list__item-button");
+  [...allIcons].forEach((icon) => {
+    icon.style.filter = "grayscale(100%)";
+    icon.style.cursor = "not-allowed";
+  });
+  form.removeEventListener("submit", addItem);
+  form.addEventListener("submit", saveItem);
+  formInput.value = item.querySelector(".list__item-text").innerText;
+  formButton.innerText = "Save";
+
+  // Save edited item
+  function saveItem() {
+    event.preventDefault();
+    itemText = formInput.value;
+    item.querySelector(".list__item-text").innerText = itemText;
+    formButton.innerText = "Add";
+    form.removeEventListener("submit", saveItem);
+    form.addEventListener("submit", addItem);
+    const allIcons = document.getElementsByTagName("img");
+    [...allIcons].forEach((icon) => {
+      icon.style.filter = "none";
+      icon.style.cursor = "pointer";
+    });
+  }
 }
 
 // Delete item from a list
