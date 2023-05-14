1f971281582183da7e501c3cd2656b8d632ce53a
Volodymyr Donets <volodons@gmail.com>
2023-05-14 11:21:13 +0300

    Refactor and optimize JS code

--- task2_6/scripts/cookies.js
+++ task2_6/scripts/cookies.js
@@ -5,6 +5,18 @@ function getCookies(name) {
       .find((cookie) => cookie.trim().startsWith(name + "="))
       .split("=")[1];
     return cookies;
+  } else {
+    const items = [];
+    const itemsJSON = JSON.stringify(items);
+    const date = new Date();
+    date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${itemsJSON};` + expires + "path=/";
+    const cookies = document.cookie
+      .split(";")
+      .find((cookie) => cookie.trim().startsWith(name + "="))
+      .split("=")[1];
+    return cookies;
   }
 }
 
--- task2_6/scripts/script.js
+++ task2_6/scripts/script.js
@@ -12,8 +12,12 @@ const deleteAllButton = document.getElementById("deleteAllButton");
 formInput.focus();
 
 // Add event listeners to form and button
-form.addEventListener("submit", addItem);
-deleteAllButton.addEventListener("click", deleteAllItems);
+form.addEventListener("submit", (event) =>
+  getInput(event, todoItems, todoItemsList, todoCookiesName)
+);
+deleteAllButton.addEventListener("click", () =>
+  deleteAllItems(todoItems, todoItemsList, todoCookiesName)
+);
 
 // Set name of ToDo cookies
 const todoCookiesName = "todo-items";
@@ -24,55 +28,61 @@ let todoItems = JSON.parse(getCookies(todoCookiesName));
 // Set flag for editing operations
 let editButtonIsActivated = false;
 
-function renderItems(items, container) {
-  if (document.cookie.includes(todoCookiesName)) {
-    container.innerHTML = "";
-    for (let i = 0; i < items.length; i++) {
-      const todoItemIndex = items.indexOf(items[i]);
-      const todoItemHTML = document.createElement("li");
-      todoItemHTML.classList.add("list__item");
-      todoItemHTML.innerHTML = `
+function renderItems(items, container, cookiesName) {
+  container.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    const itemIndex = i;
+    const itemHTML = document.createElement("li");
+    itemHTML.classList.add("list__item");
+    itemHTML.innerHTML = `
       <span class="list__item-text">${items[i]}</span>
         <img
-          id="editButton${todoItemIndex}"
+          id="editButton${itemIndex}"
           class="list__item-icon"
           src="./images/icon-edit.svg"
           alt="Edit Item"
           title="Edit Item"
         />
         <img
-          id="deleteButton${todoItemIndex}"
+          id="deleteButton${itemIndex}"
           class="list__item-icon"
           src="./images/icon-delete.svg"
           alt="Delete Item"
           title="Delete Item"
         />`;
-      container.append(todoItemHTML);
-      const editButton = document.getElementById(`editButton${todoItemIndex}`);
-      editButton.addEventListener("click", () => editItem(todoItemIndex));
-      const deleteButton = document.getElementById(
-        `deleteButton${todoItemIndex}`
-      );
-      deleteButton.addEventListener("click", () => deleteItem(todoItemIndex));
-    }
+    container.append(itemHTML);
+    const editButton = document.getElementById(`editButton${itemIndex}`);
+    editButton.addEventListener("click", () =>
+      editItem(items, container, itemIndex, cookiesName)
+    );
+    const deleteButton = document.getElementById(`deleteButton${itemIndex}`);
+    deleteButton.addEventListener("click", () =>
+      deleteItem(items, container, itemIndex, cookiesName)
+    );
+  }
+}
+
+// Get data from client input
+function getInput(event, items, container, cookiesName) {
+  event.preventDefault();
+  if (formInput.value && !/^\s*$/.test(formInput.value)) {
+    const formData = new FormData(event.target);
+    const input = formData.get("item");
+    formInput.value = "";
+    formInput.focus();
+    addItem(input, items, container, cookiesName);
   }
 }
 
 // Add new item to list
-function addItem(event) {
-  event.preventDefault();
-  if (formInput.value && !/^\s*$/.test(formInput.value)) {
-    const todoItem = formInput.value;
-    todoItems.push(todoItem);
-    updateCookies(todoCookiesName, todoItems);
-    renderItems(todoItems, todoItemsList);
-    formInput.value = "";
-    formInput.focus();
-  }
+function addItem(item, items, container, cookiesName) {
+  todoItems.push(item);
+  updateCookies(cookiesName, items);
+  renderItems(items, container, cookiesName);
 }
 
 // Edit item
-function editItem(todoItemIndex) {
+function editItem(items, container, itemIndex, cookiesName) {
   if (!editButtonIsActivated) {
     const allIcons = document.getElementsByTagName("img");
     [...allIcons].forEach((icon) => {
@@ -84,18 +94,19 @@ function editItem(todoItemIndex) {
     form.removeEventListener("submit", addItem);
     form.addEventListener("submit", saveItem);
     formInput.focus();
-    formInput.value = todoItems[todoItemIndex];
+    formInput.value = items[itemIndex];
     formButton.innerText = "Save";
     editButtonIsActivated = true;
   }
 
   // Save edited item
-  function saveItem() {
+  function saveItem(event) {
     event.preventDefault();
-    const todoItem = formInput.value;
-    todoItems[todoItemIndex] = todoItem;
-    updateCookies(todoCookiesName, todoItems);
-    renderItems(todoItems, todoItemsList);
+    const formData = new FormData(event.target);
+    const item = formData.get("item");
+    items[itemIndex] = item;
+    updateCookies(cookiesName, items);
+    renderItems(items, container);
     formButton.innerText = "Add";
     form.removeEventListener("submit", saveItem);
     form.addEventListener("submit", addItem);
@@ -112,22 +123,22 @@ function editItem(todoItemIndex) {
 }
 
 // Delete item from a list
-function deleteItem(todoItemIndex) {
+function deleteItem(items, itemsList, itemIndex, cookiesName) {
   if (!editButtonIsActivated) {
-    delete todoItems[todoItemIndex];
-    todoItems = todoItems.filter(Boolean);
-    updateCookies(todoCookiesName, todoItems);
-    renderItems(todoItems, todoItemsList);
+    delete items[itemIndex];
+    items = items.filter(Boolean);
+    updateCookies(cookiesName, items);
+    renderItems(items, itemsList);
   }
 }
 
 // Delete all items in list
-function deleteAllItems() {
+function deleteAllItems(items, itemsList, cookiesName) {
   if (!editButtonIsActivated) {
-    todoItemsList.innerHTML = "";
-    todoItems = [];
-    updateCookies(todoCookiesName, todoItems);
+    itemsList.innerHTML = "";
+    items = [];
+    updateCookies(cookiesName, items);
   }
 }
 
-renderItems(todoItems, todoItemsList);
+renderItems(todoItems, todoItemsList, todoCookiesName);
